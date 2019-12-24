6aa5a8fa9d4428da670e0e0b425a3614193e2a80
Urbano Baz <urbanobaz@Urbanos-iMac.attlocal.net>
2019-12-24 12:58:47 -0500

    Shifting content in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => {
     <div>
       <Layout>
         <Head title="Home" />
-        <h1>Hello.</h1>
-        <h2>I'm Urbano from sunny Miami, FL. I love web dev and I'm also trying to learn Chinese (Mandarin). 你好</h2>
+        <h1>Hello. 你好</h1>
+        <h2>I'm Urbano from sunny Miami, FL. I love web dev and I'm also trying to learn Chinese (Mandarin).</h2>
         <p>Need a developer <Link to="/contact">Contact me.</Link></p>
         <p>Follow me on <a href="https://twitter.com/ubaz_3">Twitter</a></p>
       </Layout>
